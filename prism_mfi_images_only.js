5c1684616b1198b5173c41c3d2d94455c922fd5b
afullhart <58483164+afullhart@users.noreply.github.com>
2024-10-01 10:39:26 -0700

    Create prism_mfi_images_only.js

---
+++ prism_mfi_images_only.js
@@ -0,0 +1,32 @@
+//Modified Fournier Index
+//The js code editor will show a visualization. No image is exported/no task.
+
+var prism_ic = ee.ImageCollection('OREGONSTATE/PRISM/AN81m');
+
+var start_year = 1990;
+var end_year = 2019;
+var start = ee.Date.fromYMD(start_year, 1, 1);
+var end = ee.Date.fromYMD(end_year+1, 1, 1);
+var prism_ic = prism_ic.filterDate(start, end);
+
+var month_list = ee.List([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]);
+
+function month_fn(mo){
+  var month_ic = prism_ic.filter(ee.Filter.calendarRange(mo, mo, 'month'));
+  var mean_im = month_ic.select('ppt').reduce(ee.Reducer.mean());
+  return mean_im;
+}
+
+var month_ic = ee.ImageCollection(month_list.map(month_fn));
+var annual_im = month_ic.sum();
+
+function squared_fn(mo_im){
+  var sqr_im = mo_im.multiply(mo_im);
+  return sqr_im;  
+}
+
+var sqr_ic = month_ic.map(squared_fn);
+var sqrsum_im = sqr_ic.sum();
+var mfi_im = sqrsum_im.divide(annual_im);
+Map.addLayer(mfi_im, null, 'mfi');
+
